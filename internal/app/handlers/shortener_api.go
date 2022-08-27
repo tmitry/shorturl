bed6b5de81631075799a0b986efb032b6315f9fa
Tima Dushkevich <tmitry@gmail.com>
2022-08-27 21:21:02 +0400

    Increment 4

---
+++ internal/app/handlers/shortener_api.go
@@ -0,0 +1,101 @@
+package handlers
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+
+	"github.com/tmitry/shorturl/internal/app/models"
+	"github.com/tmitry/shorturl/internal/app/repositories"
+	"github.com/tmitry/shorturl/internal/app/util"
+)
+
+type requestJSON struct {
+	URL models.URL `json:"url"`
+}
+
+func NewRequestJSON() *requestJSON {
+	return &requestJSON{
+		URL: "",
+	}
+}
+
+type responseJSON struct {
+	Result models.URL `json:"result"`
+}
+
+func NewResponseJSON(url models.URL) *responseJSON {
+	return &responseJSON{
+		Result: url,
+	}
+}
+
+type ShortenerAPIHandler struct {
+	Rep repositories.Repository
+}
+
+func NewShortenerAPIHandler(rep repositories.Repository) *ShortenerAPIHandler {
+	return &ShortenerAPIHandler{
+		Rep: rep,
+	}
+}
+
+func (h ShortenerAPIHandler) Shorten(writer http.ResponseWriter, request *http.Request) {
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+			http.Error(writer, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+			log.Println(err.Error())
+		}
+	}(request.Body)
+
+	requestJSON := NewRequestJSON()
+	if err := json.NewDecoder(request.Body).Decode(requestJSON); err != nil {
+		http.Error(
+			writer,
+			fmt.Sprintf("%s: %s", http.StatusText(http.StatusBadRequest), messageIncorrectJSON),
+			http.StatusBadRequest,
+		)
+
+		return
+	}
+
+	if !requestJSON.URL.IsValid() {
+		http.Error(
+			writer,
+			fmt.Sprintf("%s: %s", http.StatusText(http.StatusBadRequest), messageIncorrectURL),
+			http.StatusBadRequest,
+		)
+
+		return
+	}
+
+	id := h.Rep.ReserveID()
+	shortURL := models.NewShortURL(id, requestJSON.URL, models.UID(util.GenerateUniqueHash(id)))
+
+	h.Rep.Save(shortURL)
+
+	writer.Header().Set("Content-Type", ContentTypeJSON)
+	writer.WriteHeader(http.StatusCreated)
+
+	responseJSON := NewResponseJSON(shortURL.GetShortURL())
+
+	var buf bytes.Buffer
+	jsonEncoder := json.NewEncoder(&buf)
+	jsonEncoder.SetEscapeHTML(false)
+
+	err := jsonEncoder.Encode(responseJSON)
+	if err != nil {
+		http.Error(writer, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		log.Println(err.Error())
+	}
+
+	_, err = buf.WriteTo(writer)
+	if err != nil {
+		http.Error(writer, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		log.Println(err.Error())
+	}
+}
---
+++ internal/app/handlers/shortener_api_test.go
@@ -0,0 +1,97 @@
+package handlers
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"github.com/tmitry/shorturl/internal/app/config"
+	"github.com/tmitry/shorturl/internal/app/repositories"
+)
+
+func TestShortenerAPIHandler_Shorten(t *testing.T) {
+	t.Parallel()
+
+	rep := repositories.NewMemoryRepository()
+	handler := NewShortenerAPIHandler(rep)
+
+	type want struct {
+		contentType string
+		statusCode  int
+		content     string
+	}
+
+	tests := []struct {
+		name string
+		json string
+		want want
+	}{
+		{
+			name: "bad JSON",
+			json: "bad_json",
+			want: want{
+				contentType: ContentTypeText,
+				statusCode:  http.StatusBadRequest,
+				content:     fmt.Sprintf("%s: %s", http.StatusText(http.StatusBadRequest), messageIncorrectJSON),
+			},
+		},
+		{
+			name: "bad JSON format",
+			json: `{"ur_l":"https://example.com/"}`,
+			want: want{
+				contentType: ContentTypeText,
+				statusCode:  http.StatusBadRequest,
+				content:     fmt.Sprintf("%s: %s", http.StatusText(http.StatusBadRequest), messageIncorrectURL),
+			},
+		},
+		{
+			name: "bad URL",
+			json: `{"url":"bad_url"}`,
+			want: want{
+				contentType: ContentTypeText,
+				statusCode:  http.StatusBadRequest,
+				content:     fmt.Sprintf("%s: %s", http.StatusText(http.StatusBadRequest), messageIncorrectURL),
+			},
+		},
+		{
+			name: "correct URL",
+			json: `{"url":"https://example.com/"}`,
+			want: want{
+				contentType: ContentTypeJSON,
+				statusCode:  http.StatusCreated,
+				content:     "",
+			},
+		},
+	}
+	for _, testCase := range tests {
+		testCase := testCase
+		t.Run(testCase.name, func(t *testing.T) {
+			t.Parallel()
+
+			request := httptest.NewRequest(http.MethodPost, config.DefaultAddr, strings.NewReader(testCase.json))
+			request.Header.Set("Content-Type", ContentTypeJSON)
+
+			recorder := httptest.NewRecorder()
+
+			handler.Shorten(recorder, request)
+			result := recorder.Result()
+
+			assert.Equal(t, testCase.want.statusCode, result.StatusCode)
+			assert.Equal(t, testCase.want.contentType, GetContentType(result))
+
+			content, err := ioutil.ReadAll(result.Body)
+			require.NoError(t, err)
+			err = result.Body.Close()
+			require.NoError(t, err)
+
+			if result.StatusCode != http.StatusCreated {
+				assert.Equal(t, testCase.want.content, strings.TrimSuffix(string(content), "\n"))
+			}
+		})
+	}
+}
